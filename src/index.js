24d40d5276aec0586e60bc3e6f252f6a48bb359a
carmentenorio <carmen.tenorio.btsoft@gmail.com>
2025-10-28 23:23:55 +0100

    PBN-1:Initialize the base project (#1)

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Server working correctly' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
